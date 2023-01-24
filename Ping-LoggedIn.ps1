ebd703668f7a73fd94e854c2f881ab79d416a633
kyle-griffith <griffithdesigned@gmail.com>
2023-01-24 14:56:11 -0800

    added Ping-LoggedIn function

---
+++ Ping-LoggedIn.ps1
@@ -0,0 +1,16 @@
+# Allow Admin Privileges -- may need to configure Set-ExecutionPolicy -Scope CurrentUser
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+function Ping-LoggedIn {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$True)]
+        [string[]]$computername
+    )
+
+    foreach ($pc in $computername) {
+        $logged_in = (gwmi win32_computersystem -COMPUTER $pc).username 
+        $name = $logged_in.split("\")[1]
+        "{0}: {1}" -f $pc, $name
+    }
+}
