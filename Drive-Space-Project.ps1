8ea71ec53c72e2cbe2171cf184a9845abadefc72
kyle-griffith <griffithdesigned@gmail.com>
2023-01-25 09:10:06 -0800

    renamed tutorial, started project in drive-space-project.ps1

--- Drive-Space-Project.ps1
+++ Drive-Space-Project.ps1
@@ -1,99 +1,31 @@
-# Tutorial from TechThoughts (https://www.youtube.com/watch?v=IABNJEl2ZWk)
-
-<#
-Will need to support logging
-Checks drive for space
-Lets user specify driver
-Supports Windows & Linux
-Sends Telegram notification
+<# 
+I want to take what I learned from the last tutorial and apply it here: 
+Scan a selected drive on a Windows machine
+Return the space available
+Push an email when storage is at 75% max capacity
 #>
 
-# Log Directory
-if ($PSVersionTable.Platform -eq 'Unix') {
-    $logPath = '/tmp'
-}
-else {
-    $logPath = 'C:\Logs'
-}
+# Need a Cmdlet to determine drive space 
+# Need an `if` statement to handle the logic 
+# Need an SMPT server to handle the email + an email subject
+# Use `Send-MailMessage` Cmdlet to deliver email
 
-$logFile = "$logPath\driveCheck.log" #log file here
-
-# Verify if Log Directory exists
-try {
-    if (-not (Test-Path -Path $logPath -ErrorAction Stop)) {
-        #output dir is not found -- Creat the dir
-        New-Item -ItemType Directory -Path $logPath -ErrorAction Stop | Out-Null #Out-Null prevents console output
-        New-Item -ItemType -Path $logFile -ErrorAction | Out-Null 
-    }
-}
-
-catch {
-    throw #cancels the process
+$diskUsage = (Get-Volume).FreeSpace / (Get-Volume).Size
+if ($diskUsage -le 0.25) {
+    # Declare variables for the email
+    $to = "mementovivereburner@gmail.com"
+    $subject = "Disk Space Warning"
+    $body = "Your disk is at or above 75% capacity. Please free up some space."
+    $smtpServer = "smtp.gmail.com"
+    $smtpPort = "587"
+    $username = ""
+    $password = ""
+    $smtpCredentials = New-Object
+    System.Management.Automation.PSCredential($username, (ConvertTo-SecureString $password -AsPlainText -Force))
+    # Send the message
+    Send-Mail -To $to -Subject $subject -Body $body -SmtpServer $smtpServer -Port $smtpPort -UseSsl -Credential $smtpCredentials
 }
 
-Add-Content -Path $logFile -Value "[INFO] Running $PSCommandPath" #tells log where the script was ran from to be logged
-
-# Verify that PoshGram is installed
-if (-not (Get-Module -Name PoshGram -ListAvailable)) {
-    Add-Content -Path $logFile -Value "[ERROR] PoshGram is not installed."
-    throw #cancels script altogether
-} #press F8 to run script and verify
-
-else {
-    Add-Content -Path $logFile -Value "[INFO] PoshGram is installed."
-}
-
-# Get hard drive information
-try {
-if ($PSVersionTable.Platform -eq 'Unix') {
-    #used
-    #free
-    $volume = Get-PSDrive -Name $Drive -ErrorAction Stop 
-    #verify volume exists
-    if ($volume) {
-        $total = $volume.Used + $volume.Free
-        $percentFree = [int](($volume.Free / $total) * 100)
-        Add-Content -Path $logFile -Value "[INFO] Percent Free: $percentFree%."
-    }
-    else {
-        Add-Content -Path $logFile -Value "[ERROR] $Drive was not found."
-        throw
-    }
-}
-else {
-    $volume = Get-Volume -ErrorAction Stop | Where-Object {$_.DriveLetter -eq $Drive}
-    f ($volume) {
-        $total = $volume.Size
-        $percentFree = [int](($volume.SizeRemaining / $total) * 100)
-        Add-Content -Path $logFile -Value "[INFO] Percent Free: $percentFree%."
-    }
-    else {
-        Add-Content -Path $logFile -Value "[ERROR] $Drive was not found."
-        throw
-    }
-}
-}
-catch {
-    Add-Content -Path $logFile -Value "[ERROR] Unable to retrieve volume information."
-    Add-Content -Path $logFile -Value $_
-    throw
-}
-
-
-# Send Telegram message *if the drive is low
-if ($percentFree -le 20) {
-    try {
-        Import-Module -Name PoshGram -ErrorAction Stop
-        Add-Content -Path $logFile -Value "[INFO] Imported PoshGram successfully."
-    }
-    catch {
-        Add-Content -Path $logFile -Value "[ERROR] PoshGram could not be imported:"
-        Add-Content - path $logFile -Value $_
-    }
-
-    Add-Content -Path $logFile -Value "[INFO] Sending Telegram Notification..."
-
-    $botToken = 'nnnnnnnnn:xxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxx'
-    $chat = '-nnnnnnnnn'
-    Send-TelegramTextMessage -BotToken $botToken -ChatID $chat -Message "Your drive is low"
-}
+# else {
+#     throw # Stop the script altogether
+# }
---
+++ drive-space-tutorial.ps1
@@ -0,0 +1,99 @@
+# Tutorial from TechThoughts (https://www.youtube.com/watch?v=IABNJEl2ZWk)
+
+<#
+Will need to support logging
+Checks drive for space
+Lets user specify driver
+Supports Windows & Linux
+Sends Telegram notification
+#>
+
+# Log Directory
+if ($PSVersionTable.Platform -eq 'Unix') {
+    $logPath = '/tmp'
+}
+else {
+    $logPath = 'C:\Logs'
+}
+
+$logFile = "$logPath\driveCheck.log" #log file here
+
+# Verify if Log Directory exists
+try {
+    if (-not (Test-Path -Path $logPath -ErrorAction Stop)) {
+        #output dir is not found -- Creat the dir
+        New-Item -ItemType Directory -Path $logPath -ErrorAction Stop | Out-Null #Out-Null prevents console output
+        New-Item -ItemType -Path $logFile -ErrorAction | Out-Null 
+    }
+}
+
+catch {
+    throw #cancels the process
+}
+
+Add-Content -Path $logFile -Value "[INFO] Running $PSCommandPath" #tells log where the script was ran from to be logged
+
+# Verify that PoshGram is installed
+if (-not (Get-Module -Name PoshGram -ListAvailable)) {
+    Add-Content -Path $logFile -Value "[ERROR] PoshGram is not installed."
+    throw #cancels script altogether
+} #press F8 to run script and verify
+
+else {
+    Add-Content -Path $logFile -Value "[INFO] PoshGram is installed."
+}
+
+# Get hard drive information
+try {
+if ($PSVersionTable.Platform -eq 'Unix') {
+    #used
+    #free
+    $volume = Get-PSDrive -Name $Drive -ErrorAction Stop 
+    #verify volume exists
+    if ($volume) {
+        $total = $volume.Used + $volume.Free
+        $percentFree = [int](($volume.Free / $total) * 100)
+        Add-Content -Path $logFile -Value "[INFO] Percent Free: $percentFree%."
+    }
+    else {
+        Add-Content -Path $logFile -Value "[ERROR] $Drive was not found."
+        throw
+    }
+}
+else {
+    $volume = Get-Volume -ErrorAction Stop | Where-Object {$_.DriveLetter -eq $Drive}
+    f ($volume) {
+        $total = $volume.Size
+        $percentFree = [int](($volume.SizeRemaining / $total) * 100)
+        Add-Content -Path $logFile -Value "[INFO] Percent Free: $percentFree%."
+    }
+    else {
+        Add-Content -Path $logFile -Value "[ERROR] $Drive was not found."
+        throw
+    }
+}
+}
+catch {
+    Add-Content -Path $logFile -Value "[ERROR] Unable to retrieve volume information."
+    Add-Content -Path $logFile -Value $_
+    throw
+}
+
+
+# Send Telegram message *if the drive is low
+if ($percentFree -le 20) {
+    try {
+        Import-Module -Name PoshGram -ErrorAction Stop
+        Add-Content -Path $logFile -Value "[INFO] Imported PoshGram successfully."
+    }
+    catch {
+        Add-Content -Path $logFile -Value "[ERROR] PoshGram could not be imported:"
+        Add-Content - path $logFile -Value $_
+    }
+
+    Add-Content -Path $logFile -Value "[INFO] Sending Telegram Notification..."
+
+    $botToken = 'nnnnnnnnn:xxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    $chat = '-nnnnnnnnn'
+    Send-TelegramTextMessage -BotToken $botToken -ChatID $chat -Message "Your drive is low"
+}
