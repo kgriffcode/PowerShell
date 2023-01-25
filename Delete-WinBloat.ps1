7652a24f963601a92eda033ab6da83cafd1c0971
kyle-griffith <griffithdesigned@gmail.com>
2023-01-25 09:45:57 -0800

    added more delete scripts

--- Delete-WinBloat.ps1
+++ Delete-WinBloat.ps1
@@ -1,4 +1,5 @@
 # Removes bloat programs that are normally difficult to remove from Windows 
+# Use winget list to view installed apps
 
 # Remove Skype
 Get-AppxPackage *skypeapp* | Remove-AppxPackage
@@ -14,3 +15,18 @@ Get-AppxPackage *zunemusic* | Remove-AppxPackage
 
 # Remove 3D Viewer / Builder
 Get-AppxPackage *3dbuilder* | Remove-AppxPackage
+
+# Remove Microsoft News
+Get-AppxPackage Microsoft.BingNews | Remove-AppxPackage
+
+# Remove Microsoft Maps
+Get-AppxPackage Microsoft.WindowsMaps | Remove-AppxPackage
+
+# Remove Xbox Gaming Bar
+Get-AppxPackage Microsoft.XboxGamingOverlay | Remove-AppxPackage
+
+# Remove Microsoft Edge 
+Get-AppxPackage Microsoft.Edge | Remove-AppxPackage
+
+# Remove Microsoft Mixed Reality Portal
+Get-AppxPackage Microsoft.MixedReality.Portal | Remove-AppxPackage
